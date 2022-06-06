0f216ecb3ce57e8b2269f5a0a830b52425bc1f53
Poranut Chollavorn <pipech@gmail.com>
2022-06-06 04:06:30 +0000

    feat: add paper settings

--- print_utils/layout.py
+++ print_utils/layout.py
@@ -1,11 +1,47 @@
 import frappe
+from .paper_size import paper_size_map
 
 
-def get_print_layout(content, papersize=None, margin=None):
+def get_print_layout(
+    content,
+    paper_size='A4',
+    orientation='Portrait',
+    margin=10,
+):
+    """
+    Args:
+        content (str): content to be printed in html format
+        paper_size (str|dict): standard paper size ('A4', 'Letter', ...)
+            or custom paper size in mm {'width': x, 'height': y}
+        orientation (str): orientation of the paper ('Portrait' or 'Landscape')
+        margin (int): margin in mm
+
+    Return:
+        (str): html layout
+    """
+    # get paper size
+    if type(paper_size) == str:
+        paper_size = paper_size.upper()
+        paper_dimension = paper_size_map.get(paper_size)
+        paper_width = paper_dimension.get('mm')[0]
+        paper_height = paper_dimension.get('mm')[1]
+    else:
+        paper_width = paper_size.get('width')
+        paper_height = paper_size.get('height')
+
+    # set paper size based on orientation
+    orientation = orientation.lower()
+    if orientation == 'landscape':
+        paper_width, paper_height = paper_height, paper_width
+
+    # render template
     html = frappe.render_template(
         template='print_utils/templates/print_formats/base/base_template.html.j2',
         context={
             'body': content,
+            'width': paper_width,
+            'height': paper_height,
+            'margin': margin,
         },
         is_path=True,
     )
